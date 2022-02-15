3f71020768dc81dfa007b36d666c2d3f4977cb89
AnetaGancheva <gancheva.aneta@gmail.com>
2022-02-15 22:58:50 +0100

    Python scraper now scrapes article content as well. New route to fetch articles with Python scraper component and add to database.

--- ParserV1.py
+++ ParserV1.py
@@ -1,5 +1,7 @@
+from calendar import month
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 URL = "https://ir.darlingii.com/news-releases?l=10"
 page = requests.get(URL)
@@ -10,9 +12,26 @@ results = soup.find_all(class_ = "wd_item")
 article = soup.find_all(class_ = "wd_item_wrapper")
 
 for article in article:
-    title = article.find_all(class_ = "wd_title")
-    date = article.find_all(class_ = "wd_date")
-    print(article.text)
+    titleContainer = article.find_all(class_ = "wd_title")
+    dateContainer = article.find_all(class_ = "wd_date")
+    titleAnchorLink = titleContainer[0].contents
+    titleList = titleAnchorLink[0].contents
+    dateList = dateContainer[0].contents
+    title = str(titleList[0])
+    date = str(dateList[0])
+    print(title)
+    print(date)
+    newURL = titleAnchorLink[0].get('href')
+    newPage = requests.get(newURL)
+    newSoup = BeautifulSoup(newPage.content, 'html.parser')
+    newsBody = newSoup.find_all(class_ = "wd_news_body")
+    articleText = newsBody[0].find_all('p')
+    textParsed = "";
+    for paragraph in articleText:
+        textParsed += paragraph.get_text()
+    print(textParsed)
+
+
 
 
 
--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const Sentiment = require('sentiment');
 const sentiment = new Sentiment();
-
+/* python shell */
+const {PythonShell} = require('python-shell');
 
 require('dotenv').config();
 
@@ -38,6 +39,159 @@ const newSchema = new mongoose.Schema({
 /*** Define model for news items */
 
 const news = mongoose.model('news', newSchema);
+
+/**** Get Articles from GNews API */
+
+app.get('/getAPIResponse', (request, response) => {
+    let url = process.env.URL;
+    let newsArticles = [];
+    fetch(url)
+        .then((response) => {
+            return response.json();
+            })
+        .then((data) =>{
+            const newsItems = [... data.articles];
+            console.log(newsItems.length);
+            const numberOfNewsItems = newsItems.length;
+            for(let i=0; i<numberOfNewsItems;i++) {
+                let title = newsItems[i].title;
+                let date = newsItems[i].publishedAt;
+                let text = newsItems[i].content;
+                newsArticles.push({
+                    title: title,
+                    date: date,
+                    text: text
+                })
+            }
+            return newsArticles;
+            })
+        .then((newsArticles) => {
+            //console.log(newsArticles[0]);
+            for(let i=0; i<newsArticles.length; i++){
+                let newsPiece = new news();
+                newsPiece.title = newsArticles[i].title;
+                newsPiece.date = newsArticles[i].date;
+                newsPiece.text = newsArticles[i].text;
+                let textTest = newsArticles[i].text;
+                let articleSentiment = sentiment.analyze(textTest);
+                console.log(articleSentiment.score);
+                newsPiece.positivityScore = articleSentiment.score;
+                newsPiece.save().then(() => console.log("News Item "+i+" has been added from API component."));
+            }
+        })
+    response.render("reroute");
+});
+
+
+/**** When at Home page, render index.pug with the values of the most positive articles from DB. */
+
+app.get("/viewNews", (req, res)=>{
+    news.find({positivityScore: {$gt: 0}}, (err, newsItem) => {
+        if(!err) {
+            const numArticles = newsItem.length;
+            let newsArticles = [];
+            for(let i=0; i<numArticles; i++){
+                var articleTitle = newsItem[i].toObject().title;
+                var articleDate = newsItem[i].toObject().date;
+                var articleDateFormatted = date.format(articleDate, 'MMMM D, Y HH:MM A');
+                var articleText = newsItem[i].toObject().text;
+                var articlePositivityScore = newsItem[i].toObject().positivityScore;
+                newsArticles.push({
+                    articleTitle: articleTitle,
+                    articleDate: articleDateFormatted,
+                    articleText: articleText,
+                    score: articlePositivityScore
+                });
+            }
+            newsArticles.sort( 
+                (a,b) => (a.score > b.score) ? -1 : 1
+            );
+            console.log(newsArticles);
+            res.render("index", {
+                newsItems: newsArticles
+            });
+        } else {
+        console.log(err);
+        res.send("Please investigate the cause behind this error. Check console for error message details.");
+        }
+    })
+});
+
+/* Get articles from Python components */
+
+app.get('/getPythonNews', (req, res) => {
+    let options = {
+        mode: 'text',
+        args: [['title', 'date', 'textParsed']]
+      };
+      
+    PythonShell.run('ParserV1.py', options, function (err, results) {
+        
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        let num = 0;
+        for(let i =0; i<results.length; i+=4) {
+            console.log(results.length)
+            console.log(i)
+            let newsPiece = new news();
+            const title = results[i];
+            newsPiece.title = title;
+            /* day is not 03 but 3, so needs fix */
+            const regex = /^[A-Z][a-z]* [0-9], [0-9]*$/g;
+            const isDateOneNumber = regex.test(results[i+1]);
+            let date = "";
+            if(isDateOneNumber) {
+                date = "0".concat(results[i+1])
+            } else {
+                date = results[i+1]
+            }
+            newsPiece.date = date;
+            let newsText = "";
+            let j =0;
+            let counter = 0;
+            for(j=i+3; j<results.length;j++) {
+                newsText += results[j-1];
+                counter++;
+                if(/^[A-Z][a-z]* [0-9], [0-9]*$/.test(results[j+1]) || /^[A-Z][a-z]* [0-9][0-9], [0-9]*$/.test(results[j+1])){
+                    console.log(results[j+1]);
+                    break;
+                }
+            }
+           console.log(j);
+            if(j<=results.length-1) {i+=counter-2;}
+            else{
+                for(let k=i+2;k<results.length;k++){
+                    newsText+=results[k];
+                }
+            }
+            newsPiece.text = newsText;
+            let textTest = newsText;
+            let articleSentiment = sentiment.analyze(textTest);
+            //console.log(articleSentiment.score);
+            newsPiece.positivityScore = articleSentiment.score;
+            console.log(newsPiece);
+            newsPiece.save().then(() => {num ++; console.log("News Item "+num+" has been added from Python component.")});
+            
+        }
+    });
+    res.render("python");
+})
+
+/*** HOME PAGE  */
+
+app.get("/", (req, res)=>{
+    res.render("home");
+});
+
+
+
+app.use("/viewNews", router);
+app.use("/getAPIResponse", router);
+app.use("/", router);
+
+app.listen(process.env.PORT, () => console.log("Listening to port."));
+
+
 
 
 
@@ -81,94 +235,3 @@ newsItemNotToRender.save().then(() => console.log("News Item NOT TO RENDER Has B
 
 */
 /*** END OF TEST CASES */
-
-/**** Get Articles from GNews API */
-
-app.get('/getAPIResponse', (request, response) => {
-    let url = process.env.URL;
-    let newsArticles = [];
-    fetch(url)
-        .then((response) => {
-            return response.json();
-            })
-        .then((data) =>{
-            const newsItems = [... data.articles];
-            console.log(newsItems.length);
-            const numberOfNewsItems = newsItems.length;
-            for(let i=0; i<numberOfNewsItems;i++) {
-                let title = newsItems[i].title;
-                let date = newsItems[i].publishedAt;
-                let text = newsItems[i].content;
-                newsArticles.push({
-                    title: title,
-                    date: date,
-                    text: text
-                })
-            }
-            return newsArticles;
-            })
-        .then((newsArticles) => {
-            //console.log(newsArticles[0]);
-            for(let i=0; i<newsArticles.length; i++){
-                let newsPiece = new news();
-                newsPiece.title = newsArticles[i].title;
-                newsPiece.date = newsArticles[i].date;
-                newsPiece.text = newsArticles[i].text;
-                let textTest = newsArticles[i].text;
-                let articleSentiment = sentiment.analyze(textTest);
-                console.log(articleSentiment.score);
-                newsPiece.positivityScore = articleSentiment.score;
-                newsPiece.save().then(() => console.log("News Item "+i+" has been added."));
-            }
-        })
-    response.render("reroute");
-});
-
-
-/**** When at Home page, render index.pug with the values of the most positive articles from DB. */
-
-app.get("/viewNews", (req, res)=>{
-    news.find({positivityScore: {$gt: 0}}, (err, newsItem) => {
-        if(!err) {
-            const numArticles = newsItem.length;
-            let newsArticles = [];
-            for(let i=0; i<numArticles; i++){
-                var articleTitle = newsItem[i].toObject().title;
-                var articleDate = newsItem[i].toObject().date;
-                var articleDateFormatted = date.format(articleDate, 'MMMM D, Y HH:MM A');
-                var articleText = newsItem[i].toObject().text;
-                var articlePositivityScore = newsItem[i].toObject().positivityScore;
-                newsArticles.push({
-                    articleTitle: articleTitle,
-                    articleDate: articleDateFormatted,
-                    articleText: articleText,
-                    score: articlePositivityScore
-                });
-            }
-            newsArticles.sort( 
-                (a,b) => (a.score > b.score) ? -1 : 1
-            );
-            console.log(newsArticles);
-            res.render("index", {
-                newsItems: newsArticles
-            });
-        } else {
-        console.log(err);
-        res.send("Please investigate the cause behind this error. Check console for error message details.");
-        }
-    })
-});
-
-/*** HOME PAGE  */
-
-app.get("/", (req, res)=>{
-    res.render("home");
-});
-
-
-
-app.use("/viewNews", router);
-app.use("/getAPIResponse", router);
-app.use("/", router);
-
-app.listen(process.env.PORT, () => console.log("Listening to port."));
